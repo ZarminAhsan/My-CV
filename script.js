0a2fb47a327a93a22b2929f41a0524dbcbb583b9
Zarmin Ahsan <zarminahsan26@gmail.com>
2025-04-20 21:59:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const themeToggle = document.getElementById("theme-toggle");
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    themeToggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
